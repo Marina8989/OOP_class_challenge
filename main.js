51b101f3d58cd4bfb0a3a612e6fd16c76bc38de5
Marina8989 <marina.mill209@gmail.com>
2020-08-15 19:06:27 -0700

    class OOP

--- main.js
+++ main.js
@@ -12,6 +12,18 @@ class Vehicle{
     }
 }
 
+class Car extends Vehicle{
+    constructor(make, model, year, numWheels = 4){
+        super(make, model, year);
+        this.numWheels = numWheels;
+    }
+}
+let myFirstCar = new Car('Toyota', 'Corolla', 2005);
+console.log(myFirstCar.toString());
+console.log(myFirstCar.honk());
+console.log(myFirstCar.numWheels);
+
+
 let myFirstVehicle = new Vehicle('Honda', 'Monster Truck', 1999);
 console.log(myFirstVehicle.honk());
 console.log(myFirstVehicle.toString());
